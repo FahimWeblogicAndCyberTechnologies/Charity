f3452bbb74110a9898e0ab10089761bf8f5bf95e
Fahim <89752022+FahimWeblogicAndCyberTechnologies@users.noreply.github.com>
2022-05-27 12:48:43 -0400

    Add files via upload

---
+++ charity/include/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="css/styles.css" rel="stylesheet" type="text/css"  media="all" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Charity Org</title>
+</style>
+  </head>
+<body>
+  <header>
+    <ul>
+    <div class ="nav">
+    <li><a href="http://localhost/charity/index.php">Home</a></li>
+    <li><a href="http://localhost/charity/projects.php">Projects</a></li>
+    <li><a href="http://localhost/charity/donate.php">Donate</a></li>
+    <li><a href="http://localhost/charity/about-us.php">About-us</a></li>
+    <li><a href="http://localhost/charity/contact-us.php">Contact-us</a></li>
+    <li><a href="http://localhost/charity/faq.php">FAQ</a></li>
+    </ul>
+  </div>
+  </nav>
+</header>
+</body>
+</html>
+  
+
+ 
+
+
+
+
+    
+
+
+
---
+++ charity/index.php
@@ -0,0 +1,15 @@
+<?php include 'include/header.php'; ?>
+<style>
+<?php include 'include/Projects.php'; ?>
+<style>
+<?php include 'include/Donate.php'; ?>
+<style>
+<?php include 'include/About-us.php'; ?>
+<style>
+<?php include 'include/Contact-us.php'; ?>
+<style>
+<?php include 'include/FAQ.php'; ?>
+<style>
+<?php include 'include/faq.php'; ?>
+<style>
+
